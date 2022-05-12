2f1711f6b6b131a76fff01c3b8432fd49654c850
WellingtonJr <wferreira.bm13@gmail.com>
2022-05-12 20:43:57 -0300

    debug estudo

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,5 +1,7 @@
 package br.com.dio;
 
+import java.util.Scanner;
+
 import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
@@ -8,10 +10,20 @@ public class PrimeiroPrograma {
 		Gato gato = new Gato("a","b",2);
 		Livros livros = new Livros();
 		
-		System.out.println(gato);
+		System.out.println("Informe dois numeros:" );
 		
+		Scanner scanner = new Scanner(System.in);;
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		float divisao = divisao(a,b);
+		System.out.println(divisao);
 	}
-
+	
+	
+	public static float divisao(int a, int b) {
+		return  (float)a / (float)b;
+	}
 }
 
 class Livros{
